f0b30895e7dd1146e6f5a129d967b08b7b6cc8af
davissoncsm <davisson.charles@gmail.com>
2021-04-11 09:43:25 -0300

    If applied this commit will create Entitie layer

---
+++ app/Entities/Mail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mail extends Model
+{
+    use HasFactory;
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'mails';
+
+    /** The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mail',
+        'body'
+    ];
+
+}
