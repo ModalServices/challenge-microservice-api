b7caf22806a5af668d845df89f77a737346f1c15
davissoncsm <davisson.charles@gmail.com>
2021-04-11 09:40:07 -0300

    If applied this commit will create mail table

---
+++ database/migrations/2021_04_11_123816_create_mail_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('mail');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mail');
+    }
+}
